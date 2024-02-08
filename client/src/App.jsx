df1428f9a968a250f885c82feb9906458f64ba41
Son Dao <udemydaohsn@gmail.com>
2024-02-08 10:34:11 -0700

    create an editing and highlighted code blocks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { parseSync } from "svgson";
-import { nanoid } from "nanoid";
-import { recursiveToCamel, groupBy } from "./utils";
-import SVGElement from "./components/SVGElement";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { parseSync } from "svgson";
+// import { recursiveToCamel, groupBy } from "./utils";
+// import SVGElement from "./components/SVGElement";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import CreateComponent from "./pages/CreateComponent";
@@ -16,24 +15,24 @@ how to recursively convert an object fields from snake case to camelCase
   */
 
 const App = () => {
-  const data = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 192L256.13 320l-47.95-48M191.95 320L144 272M305.71 192l-51.55 59"/></svg>`;
+  // const data = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 192L256.13 320l-47.95-48M191.95 320L144 272M305.71 192l-51.55 59"/></svg>`;
 
-  function svgToComponent(svg) {
-    function categoryHash(item) {
-      return item.name; // hash based on name property
-    }
+  // function svgToComponent(svg) {
+  //   function categoryHash(item) {
+  //     return item.name; // hash based on name property
+  //   }
 
-    const data = parseSync(svg);
-    const { attributes, children } = data;
-    const elements = groupBy(children, categoryHash);
-    const svgObject = recursiveToCamel({ attributes, elements });
-    svgObject.elementKeys = Object.keys(elements);
-    svgObject.attributes.className = svgObject.attributes.class;
-    delete svgObject.attributes.class;
-    return svgObject;
-  }
+  //   const data = parseSync(svg);
+  //   const { attributes, children } = data;
+  //   const elements = groupBy(children, categoryHash);
+  //   const svgObject = recursiveToCamel({ attributes, elements });
+  //   svgObject.elementKeys = Object.keys(elements);
+  //   svgObject.attributes.className = svgObject.attributes.class;
+  //   delete svgObject.attributes.class;
+  //   return svgObject;
+  // }
 
-  const svg = svgToComponent(data);
+  // const svg = svgToComponent(data);
 
   return (
     <BrowserRouter>
--- client/src/pages/CreateComponent.jsx
+++ client/src/pages/CreateComponent.jsx
@@ -1,5 +1,42 @@
+// import Editor from "@monaco-editor/react";
+import { useState } from "react";
+const data = `<a href="https://css.tricks.com/"></a>`;
+
 const CreateComponent = () => {
-  return <div>CreateComponent</div>;
+  const [code, setCode] = useState(data);
+  const handleChange = (event) => {
+    let newCode = event.target.value;
+    if (newCode[newCode.length - 1] == "\n") {
+      // If the last character is a newline character
+      newCode += " "; // Add a placeholder space character to the final line
+    }
+    newCode
+      .replace(new RegExp("&", "g"), "&")
+      .replace(new RegExp("<", "g"), "<");
+    setCode(newCode);
+  };
+  const handleTab = () => {};
+  return (
+    <div className="relative flex flex-row">
+      <textarea
+        className=" p-2 m-2 outline-none border-0 w-[calc(100%-2rem)] text-[15px] leading-[20px] font-mono overflow-auto resize-none z-20 bg-transparent text-black caret-slate-500"
+        name=""
+        id=""
+        // cols="30"
+        // rows="10"
+        value={code}
+        onChange={handleChange}
+        spellCheck={false}
+        onKeyDown={handleTab}
+      ></textarea>
+      <pre
+        className=" p-2 m-2 border-0 w-[calc(100%-2rem)] text-[15px] leading-[20px] font-mono overflow-auto z-10"
+        aria-hidden={true}
+      >
+        <code>{code}</code>
+      </pre>
+    </div>
+  );
 };
 
 export default CreateComponent;
